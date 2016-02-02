99599924f81c8743602b240f57b5d2623e5d076d
Jim Neutt23 <f360.jovejera@gmail.com>
2016-02-02 17:29:05 +0800

    echo posts via loop + custom body class

--- header.php
+++ header.php
@@ -4,7 +4,14 @@
 	<title>Jimmy Theme</title>
 	<?php wp_head(); ?>
 </head>
-<body>
+<?php
+	if( is_front_page() ):
+		$jimmy_classes = array('jimmy-class', 'my-class');
+	else :
+		$jimmy_classes = array('no-jimmy-class');
+	endif;
+?>
+<body <?php body_class($jimmy_classes); ?>
 
 	<nav>
 		<?php wp_nav_menu(array('theme_location' => 'primary')); ?>
--- index.php
+++ index.php
@@ -2,4 +2,13 @@
 
 <h1>Sample index.php file!</h1>
 
+<?php if( have_posts() ) : ?>
+	<?php while( have_posts() ) : the_post(); ?>
+		<h1><?php the_title(); ?></h1>
+		<small><?php the_time('F j, Y g:i a') .  the_category(); ?></small>
+		<?php the_content(); ?>		
+	<?php endwhile; ?>
+<?php endif; ?>
+
+
 <?php get_footer(); ?>
